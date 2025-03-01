696991658316be0c3337cf0f54a8991c1dad0dbb
sanyimo <sanyimo@gmail.com>
2025-03-01 17:03:08 +0100

    Router.php added

---
+++ Router.php
@@ -0,0 +1,60 @@
+<?php
+namespace MVC;
+class Router {
+    public array $rutasGet = [];
+    public array $rutasPost = [];
+
+    public function get($url, $fn) {
+        $this->rutasGet[$url] = $fn;
+    }
+
+    public function post($url, $fn) {
+        $this->rutasPost[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        //arreglo de rutas protegidas
+        $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
+        $urlActual = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];//PATH_INFO
+        $urlActual = strtok($urlActual, '?'); // Extrae la ruta base (ignora los parámetros)
+        
+        $metodo = $_SERVER['REQUEST_METHOD'];
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGet[$urlActual] ?? null;
+        } else {
+            $fn = $this->rutasPost[$urlActual] ?? null;
+        }
+
+        //proteger las rutas
+        if(in_array($urlActual, $rutas_protegidas) && !$auth) {
+
+           header('Location: /');
+        }
+        
+        if ( $fn ) {
+            // Call user fn va a llamar una función cuando no sabemos cual sera
+            call_user_func($fn, $this); // This es para pasar argumentos
+        } else {
+            echo "Página no encontrada";
+        }
+    }
+
+    //muestra una vista
+    public function render($view, $datos = []) {
+        // Leer lo que le pasamos  a la vista
+        foreach ($datos as $key => $value) {
+            $$key = $value;  // variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+        }
+
+        ob_start(); // Almacenamiento en memoria durante un momento...
+
+        // entonces incluimos la vista en el layout
+        include_once __DIR__ . "/views/$view.php";
+        $contenido = ob_get_clean(); // Limpia el Buffer
+        include_once __DIR__ . '/views/layout.php';
+    }
+}
